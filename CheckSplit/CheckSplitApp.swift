1120c297f61fb80e8e2849a07df7f3cd9ff827a5
dnstrach <dnstrach@gmail.com>
2023-11-13 17:18:54 -0800

    initial commit

---
+++ CheckSplit/CheckSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSplitApp.swift
+//  CheckSplit
+//
+//  Created by Dominique Strachan on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
